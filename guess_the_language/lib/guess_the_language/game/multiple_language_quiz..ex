fa77561ccb225534d531c4a56fb02837674d0e7e
Carlos S <sisnett19@gmail.com>
2019-03-27 17:51:43 -0400

    update watchedvideo references in multiple_language_quiz

--- guess_the_language/lib/guess_the_language/game/multiple_language_quiz..ex
+++ guess_the_language/lib/guess_the_language/game/multiple_language_quiz..ex
@@ -3,11 +3,11 @@ defmodule GuessTheLanguage.Game.MultipleLanguageQuiz do
     import Ecto.Changeset
 
     alias GuessTheLanguage.Game.Language
-    alias GuessTheLanguage.Game.LanguageVideo
+    alias GuessTheLanguage.Game.WatchedVideo
 
     schema "multiple_language_quiz" do
       many_to_many :language, Language, join_through: "language_choice"
-      belongs_to :language_video, LanguageVideo
+      belongs_to :watched_video, WatchedVideo
 
     end
 end
