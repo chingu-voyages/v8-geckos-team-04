b44ca3b3263e2b00fc8e261894b924af92c5f8a9
Carlos S <sisnett19@gmail.com>
2019-03-27 17:16:21 -0400

    create migration for language_choice

---
+++ guess_the_language/priv/repo/migrations/20190327183619_language_choice.exs
@@ -0,0 +1,12 @@
+defmodule GuessTheLanguage.Repo.Migrations.LanguageChoice do
+  use Ecto.Migration
+
+  def change do
+    create table(:language_choice) do
+    add :uuid, :uuid, null: false
+    add :correctness, :boolean, null: false
+    add :language_id, references(:languages)
+    add :multiple_language_quiz_id, references(:multiple_language_quiz)
+    
+  end
+end
