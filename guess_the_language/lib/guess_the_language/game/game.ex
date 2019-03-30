0577c98675800bccf6a7e8c0b192d0c998c1caac
Carlos S <sisnett19@gmail.com>
2019-03-30 04:10:06 -0400

    fix module reference to YoutubeVideo

--- guess_the_language/lib/guess_the_language/game/game.ex
+++ guess_the_language/lib/guess_the_language/game/game.ex
@@ -18,8 +18,7 @@ defmodule GuessTheLanguage.Game do
 
     def make_video(%YoutubeVideo{} = youtube_video) do
         {:ok, video} = Video.changeset(%Video{}, %{user_id: 1}) |> Repo.insert
-        youtube_video = Youtube.changeset(youtube_video, %{video_id: video.id}) |> Repo.insert
-       
+        youtube_video = YoutubeVideo.changeset(youtube_video, %{video_id: video.id}) |> Repo.insert
         video
     end
 end
