2dd0754dbed881a38b660aa2a0af772b7a043da1
Carlos S <sisnett19@gmail.com>
2019-03-30 03:30:59 -0400

    intermediate step towards make_video

--- guess_the_language/lib/guess_the_language/game/game.ex
+++ guess_the_language/lib/guess_the_language/game/game.ex
@@ -1,7 +1,7 @@
 defmodule GuessTheLanguage.Game do
     alias GuessTheLanguage.Repo
 
-    alias GuessTheLanguage.Game.Video
+    alias GuessTheLanguage.Game.{Video, YoutubeVideo}
 
     def get_video(id) do
         Repo.get(Video, id)
@@ -13,6 +13,11 @@ defmodule GuessTheLanguage.Game do
 
     def list_videos do
         Repo.all(Video)
-        |> Repo.preload(:youtube_video)
+        |> Repo.preload([:youtube_video, :user])
+    end
+
+    def make_video() do
+        {:ok, video} = Video.changeset(%Video{}, %{user_id: 1}) |> Repo.insert
+       
     end
 end
