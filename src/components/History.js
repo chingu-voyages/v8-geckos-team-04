4decf3049d1e670348db7757ed27b10226526b30
= <=>
2019-03-31 02:04:55 -0600

    Fix minor unused variable errors in console.

--- src/components/History.js
+++ src/components/History.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 // Clear localStorage
 // localStorage.clear();
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
 
-
 const Main = () => (
 
   <div>
