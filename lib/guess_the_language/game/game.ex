2ed922249fbb5c49c84a3b04896bf486b87d63bd
Carlos S <sisnett19@gmail.com>
2019-04-04 06:09:20 -0400

    modify create_video to accept maps

--- lib/guess_the_language/game/game.ex
+++ lib/guess_the_language/game/game.ex
@@ -1,8 +1,8 @@
 defmodule GuessTheLanguage.Game do
     alias GuessTheLanguage.Repo
 
-    alias GuessTheLanguage.Game.{Video, YoutubeVideo}
-
+    alias GuessTheLanguage.Game.{Video, YoutubeVideo, YoutubeChannel}
+  
     def get_video(id) do
         Repo.get(Video, id)
     end
@@ -16,15 +16,61 @@ defmodule GuessTheLanguage.Game do
         |> Repo.preload([:youtube_video, :user])
     end
 
-    def get_youtube_video_by(params) do
-        Repo.get_by(YoutubeVideo, params)
+
+    def get_youtube_video_by(id) do
+        Repo.get(YoutubeVideo, id)
     end
 
-    def create_video(%YoutubeVideo{} = youtube_video) do
-        youtube_video = youtube_video |> Repo.preload([:video, :youtube_channel])
+
+    def get_youtube_channel(id) do
+        Repo.get(YoutubeChannel, id)
+    end
+
+    def get_youtube_channel_by(params) do
+        Repo.get_by(YoutubeChannel, params)
+    end
+
+
+    def preload_youtube_video(youtube_video), do: youtube_video |> Repo.preload([:video, :youtube_channel])
+
+   def list_channels do
+        Repo.all(YoutubeChannel)
+        |> Repo.preload([:youtube_video])
+   end
+
+    def get_or_create_youtube_channel(youtube_channel_uuid, youtube_channel_name) do
+        case get_youtube_channel_by([youtube_uuid: youtube_channel_uuid]) do
+            nil -> YoutubeChannel.changeset(%YoutubeChannel{}, %{"youtube_uuid" => youtube_channel_uuid, "name" => youtube_channel_name}) |> Repo.insert
+            channel -> {:ok, channel}
+            end
+    end
+
+    def create_youtube_video(
+        %{
+        "youtube_video_uuid" => youtube_video_uuid,
+        "title" => title,
+        "description" => description,
+        "published_at" => time,
+        "youtube_channel_uuid" => youtube_channel_uuid,
+        "youtube_channel_name" => youtube_channel_name}) do
+
+        {:ok, youtube_channel} = get_or_create_youtube_channel(youtube_channel_uuid, youtube_channel_name)
+
+
+        %{%YoutubeVideo{} |
+         youtube_uuid: youtube_video_uuid, title: title, description: description, published_at: time, youtube_channel_id: youtube_channel.id}
+    end
+
+    def create_youtube_video(%{"youtube_video_uuid" => video_uuid, "title" => title, "description" => description, "published_at" => time}) do
+        %{%YoutubeVideo{} | youtube_uuid: video_uuid, title: title, description: description, published_at: time, youtube_channel_id: 1}
+    end
+
+
+    def create_video(%{} = params) do
+        youtube_video = create_youtube_video(params) |> preload_youtube_video
         {:ok, video} = Video.changeset(%Video{}, %{"user_id" =>  1}) |> Repo.insert
-        youtube_video = %{youtube_video | video_id: video.id, youtube_channel_id: 1}
-        {:ok, youtube_video} = YoutubeVideo.changeset(youtube_video, %{"video_id" => video.id, "youtube_channel_id" => 1}) |> Repo.insert
+        youtube_video = %{youtube_video | video_id: video.id}
+        {:ok, youtube_video} = YoutubeVideo.changeset(youtube_video) |> Repo.insert
         {video, youtube_video}
     end
 end
