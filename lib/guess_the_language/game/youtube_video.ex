339fe91fbd4294322c7d219f103f8ab3c84a47ce
Carlos S <sisnett19@gmail.com>
2019-04-04 18:04:56 -0400

    create get_or_insert youtube video function

--- lib/guess_the_language/game/youtube_video.ex
+++ lib/guess_the_language/game/youtube_video.ex
@@ -1,7 +1,7 @@
 defmodule GuessTheLanguage.Game.YoutubeVideo do
   use Ecto.Schema
   import Ecto.Changeset
-
+  alias GuessTheLanguage.Game
   alias GuessTheLanguage.Game.{Video, YoutubeChannel, YoutubeVideo}
   @derive {Jason.Encoder, only: [:youtube_uuid, :title, :description, :published_at]}
 
@@ -16,10 +16,20 @@ defmodule GuessTheLanguage.Game.YoutubeVideo do
 
   end
 
+  def get_or_insert_video(youtube_video) do
+    youtube_video
+    |> Repo.insert
+    |> case do
+      {:ok, youtube_video} -> youtube_video
+      {:error, changeset} -> Repo.get_by(YoutubeVideo, youtube_uuid: changeset.changes.youtube_uuid)
+    end
+  end
+
   def changeset(youtube_video, params \\ %{}) do
       #add validation to truncate to seconds the datetime
       youtube_video
       |> cast(params, [:youtube_uuid, :title, :description, :published_at])
       |> validate_required([:youtube_uuid, :title, :description, :published_at])
+      |> unique_constraint(:youtube_uuid)
   end
 end
