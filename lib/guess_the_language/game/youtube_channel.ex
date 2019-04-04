515cc7ff9eb0647f995ec74215b344f318670211
Carlos S <sisnett19@gmail.com>
2019-04-04 06:07:58 -0400

    add changeset to youtube channel

--- lib/guess_the_language/game/youtube_channel.ex
+++ lib/guess_the_language/game/youtube_channel.ex
@@ -7,8 +7,14 @@ defmodule GuessTheLanguage.Game.YoutubeChannel do
     schema "youtube_channel" do
       field :youtube_uuid, :string
       field :name, :string
-
       has_many :youtube_video, YoutubeVideo
+      end
 
-end
+    def changeset(youtube_channel, params \\ %{}) do
+        #add validation to truncate to seconds the datetime
+        youtube_channel
+        |> cast(params, [:youtube_uuid, :name])
+        |> validate_required([:youtube_uuid, :name])
+    end
+
 end
