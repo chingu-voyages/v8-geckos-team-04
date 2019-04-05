60dc646b74d58f62fa64a23e75ba01a96745139b
Carlos S <sisnett19@gmail.com>
2019-04-05 15:38:54 -0400

    rename watched_video table to language_video

--- priv/repo/migrations/20190404033154_rename_watched_videos__table.exs
+++
@@ -1,7 +0,0 @@
-defmodule GuessTheLanguage.Repo.Migrations.RenameWatchedVideos_Table do
-  use Ecto.Migration
-
-  def change do
-    rename table(:watched_video), to: table(:language_video)
-  end
-end
