352bf38243aee2c2285d2923c7ac42d8e7c7f2cd
Ying Huang <yinghuang1016@gmail.com>
2019-04-07 19:12:42 -0400

    Add test for React Router

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { mount } from '../../enzyme';
+import { MemoryRouter } from 'react-router';
+import Main from '../components/Main';
+import Game from '../components/Game';
 import App from '../components/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test("path '/' should redirect to Main page", () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={[ '/' ]}>
+      <App/>
+    </MemoryRouter>
+  );
+  expect(wrapper.find(Main)).toHaveLength(1);
+  expect(wrapper.find(Game)).toHaveLength(0);
 });
