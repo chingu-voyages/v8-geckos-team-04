f9207decb3e52c05b23d1ae7cd792ab273fc7afe
Carlos S <sisnett19@gmail.com>
2019-04-07 15:04:54 -0400

    fix typo on_delete on language_name migration

--- priv/repo/migrations/20190327211645_language_name.exs
+++ priv/repo/migrations/20190327211645_language_name.exs
@@ -4,7 +4,7 @@ defmodule GuessTheLanguage.Repo.Migrations.LanguageName do
   def change do
     create table(:language_name) do
       add :name, :string, null: false
-      add :written_id, references(:language, on_delele: :delete_all), null: false
+      add :written_id, references(:language, on_delete: :delete_all), null: false
       add :target_id, references(:language, on_delete: :delete_all), null: false
     end
   end
