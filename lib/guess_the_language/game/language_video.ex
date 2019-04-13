7800d65bb696d63f2b64a053774a16c48d3066c4
Carlos S <sisnett19@gmail.com>
2019-04-13 03:59:19 -0400

    add start_time and end_time to language_video

--- lib/guess_the_language/game/language_video.ex
+++ lib/guess_the_language/game/language_video.ex
@@ -6,14 +6,16 @@ defmodule GuessTheLanguage.Game.LanguageVideo do
     alias GuessTheLanguage.Game.Language
 
     schema "language_video" do
+        field :start_time, :int, default: 0
+        field :end_time, :int, default: 0
         belongs_to :video, Video
         belongs_to :language, Language
     end
 
     def changeset(language_video, params \\ %{}) do
         language_video
-        |> cast(params, [:video_id, :language_id])
-        |> validate_required([:video_id, :language_id])
+        |> cast(params, [:video_id, :language_id, :start_time, :end_time])
+        |> validate_required([:video_id, :language_id, :start_time, :end_time])
         |> foreign_key_constraint(:language_id)
         |> foreign_key_constraint(:video_id)
       end
--- priv/repo/migrations/20190327212058_language_video.exs
+++ priv/repo/migrations/20190327212058_language_video.exs
@@ -3,6 +3,8 @@ defmodule GuessTheLanguage.Repo.Migrations.WatchedVideo do
 
   def change do
     create table(:language_video) do
+      add :start_time, :int, null: false
+      add :end_time, :int, null: false
       add :video_id, references(:video, on_delete: :delete_all), null: false
       add :language_id, references(:language, on_delete: :delete_all), null: false
       end
