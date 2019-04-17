66cb1391a63d0bb41d2a8aaa14269219e7b66f2f
Susanna Kline <susannakline@gmail.com>
2019-04-17 17:31:53 -0500

    Style score responsively

---
+++ build/precache-manifest.fa1e2905d08e1f13a49fd64a93865fc8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d20780832d5c4476e33",
+    "url": "/static/js/main.62696dc2.chunk.js"
+  },
+  {
+    "revision": "d35fe7d5c6634ff461dc",
+    "url": "/static/js/2.f865eab3.chunk.js"
+  },
+  {
+    "revision": "9de99479c5635c563facef5f2e0b8f13",
+    "url": "/index.html"
+  }
+];
