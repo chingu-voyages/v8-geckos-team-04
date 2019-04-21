7850ceca3812c591e78c54a2c2d685d372be5bdb
Carlos S <sisnett19@gmail.com>
2019-04-21 16:30:27 -0500

    delete/create language_quiz migration

--- priv/repo/migrations/20190327213223_language_quiz.exs
+++ priv/repo/migrations/20190327213223_language_quiz.exs
@@ -1,11 +1,11 @@
-defmodule GuessTheLanguage.Repo.Migrations.MultipleLanguageQuiz do
+defmodule GuessTheLanguage.Repo.Migrations.LanguageQuiz do
   use Ecto.Migration
 
   def change do
-    create table(:multiple_language_quiz) do
+    create table(:language_quiz) do
       add :uuid, :uuid, null: false
       add :language_video_id, references(:language_video, on_delete: :delete_all), null: false
     end
-    create unique_index(:multiple_language_quiz, [:uuid])
+    create unique_index(:language_quiz, [:uuid])
   end
 end
